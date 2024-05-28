5ff927dd75355898191e5033130ccecfa16ef227
Angel Serrato <serratoarias@yahoo.com>
2024-05-27 20:20:19 -0500

    Initial commit

---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Home' })
+});
+
+router.get('/add', (req, res) => {
+    res.render('add', { title: 'Add user' })
+});
+
+module.exports = router;
